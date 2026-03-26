8a8b57960dec3f66afcac4b02b583b8569a4272f
Default <default@email.com>
2026-03-26 19:47:56 +0300

    .

--- LiveAuction/LiveAuction.Application/Helpers/Scalar Doc/ScalarDocumentInfo.cs
+++ LiveAuction/LiveAuction.Application/Helpers/Scalar Doc/ScalarDocumentInfo.cs
@@ -41,7 +41,7 @@ namespace LiveAuction.Application.Helpers
                 ## App Version Control
                 The API strictly monitors the mobile app version via the `X-App-Version` header:
                 * **Minimum Supported Versions:** `>= 1.0.0` optinal to update
-                * **Current Version Versions:** `= 1.0.0`
+                * **Current Version Versions:** `= 1.2.0`
                 * **Banned/Deprecated Versions:** `1.3.1` and `0.3.0` forced to update
 
                 ---
